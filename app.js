58f0642ef4524a7caa328bea88474baaf50aa124
TruyenBD <mrtruyenbd1407@gmail.com>
2024-06-20 23:55:28 +0700

    update library

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-const   = require('morgan')
+const  logger  = require('morgan')
 const mongoClient = require('mongoose')
 
 // setup connect mongodb by mongoose
